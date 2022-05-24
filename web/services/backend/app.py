dd0fa9267d2dd7dccbf51935cd81dedd74d8d129
Christian Jacolot <christian.jacolot@lph.bzh>
2022-05-24 09:03:03 +0200

    Backend: ajout communication avec la Microbit server buzzer.

--- web/services/backend/app.py
+++ web/services/backend/app.py
@@ -1,6 +1,11 @@
+# coding: utf-8
+# services Web
 from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 
+# Liaison série avec la Microbit server buzzer
+# https://pyserial.readthedocs.io/en/latest/pyserial.html
+import serial
 
 # configuration
 # debug requests (in web page or insomnia)
@@ -14,6 +19,15 @@ app.config.from_object(__name__)
 # the vuejs is started using another server
 # to request Tigrouroyal API from Vuesjs (axios), CORS must be enabled.
 CORS(app, resources={r'/*': {'origins': '*'}})
+
+# definition de la liaison série
+port = "/dev/ttyACM0"  # A modifier/configurer (voir commande dmesg)
+
+baud = 115200
+# configure et ouvre le port série.
+# si ça ne marche pas : SerialException est lévée
+s = serial.Serial(port)
+s.baudrate = baud
 
 # some default values for the buzzer
 buzzer_1 = {
@@ -77,6 +91,9 @@ def activate_buzzer(buzzer_id):
     for one_buzzer in buzzer_list:
         if one_buzzer["id"] == buzzer_id:
             print(f"Buzzer activated: {buzzer_id}")
+            # envoie le message sur la Microbit server buzzer
+            message = bytes('envoi,' + buzzer_id, 'utf-8')
+            s.write(message)
             return jsonify(one_buzzer), 204
     # not found
     abort(404)
